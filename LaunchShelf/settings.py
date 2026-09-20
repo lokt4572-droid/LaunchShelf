99e236c3577ed611f55095826c793e7e29965e93
Ahmad <lokt4572@gmail.com>
2026-09-20 20:58:16 +0500

    changing ALLOWED HOSTS

--- LaunchShelf/settings.py
+++ LaunchShelf/settings.py
@@ -26,9 +26,13 @@ else:
 
 ALLOWED_HOSTS = [
     host.strip()
-    for host in os.environ.get("DJANGO_ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
+    for host in os.environ.get(
+        "DJANGO_ALLOWED_HOSTS",
+        "127.0.0.1,localhost"
+    ).split(",")
     if host.strip()
 ]
+
 
 INSTALLED_APPS = [
     "accounts",
